93eac5fc73cad6209290926b07893e883dd2ea2c
Lorenzo <lorenzo.esposito@iisviolamarchesini.edu.it>
2023-10-04 17:24:26 +0200

    ..

---
+++ ConvertDP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization.Formatters;
+
+namespace Conversioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] DP = new int[4];
+            for (int i = 0; i < DP.Length; i++)
+            {
+                Console.WriteLine($"Inserisci la {i+1} cifra");
+                DP[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            bool[] bn = ConvertDpToBin(DP);
+
+            for (int i = 0; i < bn.Length; i++)
+            {
+                Console.Write(Convert.ToInt32(bn[i]));
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static bool[] ConvertDpToBin(int[] dp)  // 1
+        {
+            bool[] bn = new bool[32];
+
+            int indice = bn.Length - 1;
+
+            for(int i = 0; i < dp.Length; i++)
+            {
+                int num = dp[i];
+
+                for(int j = 0; j < 8; j++)
+                {
+                    bn[indice] = num % 2 == 1;
+                    num = num / 2;
+                    indice--;
+                }
+            }
+            return bn;
+        }
+
+    }
+}
---
+++ Es1Sistemi/Program.cs
@@ -0,0 +1,46 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Inserisci la targa nel formato AA000AA: ");
+        string inputTarga = Console.ReadLine();
+        string Targa = Converter(inputTarga);
+        int ValTarga = ValoreTarga(Targa);
+        Console.WriteLine($"Il valore della targa convertita {Targa} è: {ValTarga}");
+
+        Console.ReadLine();
+    }
+
+    static string Converter(string targa)
+    {
+        // Converti la targa nel formato "AAAA000".
+        return $"{targa.Substring(0, 2)}{targa.Substring(5, 2)}{targa.Substring(2, 3)}";
+    }
+
+    static int ValoreTarga(string targa)
+    {
+        int val = 0;
+        for (int i = 0; i < targa.Length; i++)
+        {
+            if (char.IsDigit(targa[i]))
+            {
+                val *= 10; //vado a vedere il sistema numerico posizionale
+                val += targa[i] - '0'; // Converto il carattere numerico in valore numerico.
+            }
+            else if (char.IsLetter(targa[i]))
+            {
+                val *= 26; // 26 lettere nell'alfabeto inglese, quindi moltiplico per 26
+                val += char.ToUpper(targa[i]) - 'A'; // Converto la lettera in maiuscolo e la trasformo in valore da 0 a 25.
+            }
+        }
+
+        return val; //ritorno la stringa
+    }
+}
